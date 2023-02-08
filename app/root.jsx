378d37846eb601f18b78bb354e272b02452fac5f
Juan Felipe Rada <j.f.r.a1997@gmail.com>
2023-02-08 12:46:27 -0300

    feat: Add stylesheets and config the meta and links in html root

--- app/root.jsx
+++ app/root.jsx
@@ -1,31 +1,76 @@
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+/**
+ * RADAPLS PROJECTS
+ * ------------------
+ * Copyright (C) 2023 Juan Felipe Rada - All Rights Reserved.
+ *
+ * This file, project or its parts can not be copied and/or distributed without
+ * the express permission of Juan Felipe Rada.
+ *
+ * @file root.jsx
+ * @author Juan Felipe Rada <radapls8@gmail.com>
+ * @date Wednesday, 8th February 2023
+ */
 
-export const meta = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+import { Links, Meta } from '@remix-run/react'
+import styles from './styles/index.css'
 
-export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+export function meta()
+{
+    return (
+        {
+            charset: 'utf-8',
+            title: 'Miusiq Remix',
+            viewport: 'width=device-width,initial-scale=1'
+        }
+    )
+}
+
+export function links()
+{
+    return [
+        {
+            rel: 'stylesheet',
+            href: 'https://necolas.github.io/normalize.css/8.0.1/normalize.css'
+        },
+        {
+            rel: 'preconnect',
+            href: 'https://fonts.googleapis.com'
+        },
+        {
+            rel: 'preconnect',
+            href: 'https://fonts.gstatic.com',
+            crossOrigin: 'true'
+        },
+        {
+            rel: 'stylesheet',
+            href: 'https://fonts.googleapis.com/css2?family=Outfit:wght@400;700;900&display=swap'
+        },
+        {
+            rel: 'stylesheet',
+            href: styles
+        }
+    ]
+}
+
+export default function App()
+{
+    return (
+        <Document>
+            <h1>Hello</h1>
+        </Document>
+    )
+}
+
+function Document({ children })
+{
+    return (
+        <html lang="en">
+            <head>
+                <Meta />
+                <Links />
+            </head>
+            <body>
+                {children}
+            </body>
+        </html>)
 }
